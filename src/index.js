9b88e2d463749eb94c1777e8e18e9c446adeb74b
Trenton Perry <trentperry08@gmail.com>
2023-10-08 09:14:18 -0800

    worked on toy database

--- src/index.js
+++ src/index.js
@@ -13,3 +13,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(toy =>{
+      const card = createToyCard(toy)
+      document.getElementById('toy-collection').appendChild(card)
+    })
+  })
+
+  function createToyCard(toy){
+    const card = document.createElement('div')
+    card.classList.add('toy-card')
+    card.innerHTML= `
+    <h2>${toy.name}<h2/>
+    <p>${toy.description}<p/>`
+    return card 
+  }
+
+  fetch('http://localhost:3000/toys', {
+    methos:"POST"
+    headers:{
+      'content-type': 'application/json'
+    }
+    body:JSON.stringify(newToy  )
+  })
